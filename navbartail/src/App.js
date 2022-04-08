f5e66abdc448d8253024cdf8be600f4e513e3a2c
cyjjj <cyd5538@naver.com>
2022-04-08 12:41:31 +0900

    tailwind practice

---
+++ navbartail/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Nav from './components/Nav';
+
+function App() {
+  return (
+    <div>
+      <Nav/>
+      
+      <div className="bg-indigo-600 w-full h-screen"></div>
+    </div>
+  );
+
+}
+
+export default App;
