963f6510037826fbc19f827d97120f580a05002e
cyjjj <cyd5538@naver.com>
2022-04-15 14:13:00 +0900

    .

--- responsive-stylecomponent/src/index.js
+++ responsive-stylecomponent/src/index.js
@@ -1,13 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
   <React.StrictMode>
     <App />
   </React.StrictMode>
-  </BrowserRouter>
 );
