b599931378a8e84f1ab9170af2644184c0234319
cyjjj <cyd5538@naver.com>
2022-05-12 10:24:30 +0900

    Framer Motion

---
+++ Framer Motion/README.md
@@ -0,0 +1,2 @@
+### Chartjs Practice
+![image](https://user-images.githubusercontent.com/91642972/167230046-bca2c7ce-a75c-46f3-a0d6-849dce49254b.png)
---
+++ Framer Motion/images.js
@@ -0,0 +1,9 @@
+import image1 from './assets/a.jpg'
+import image2 from './assets/b.jpg'
+import image3 from './assets/c.jpg'
+import image4 from './assets/d.jpg'
+import image5 from './assets/e.jpg'
+import image6 from './assets/f.jpg'
+
+
+export default [image1,image2,image3,image4,image5,image6];
