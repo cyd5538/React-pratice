7d3206803381be88d3652e08a3eebc3a5b52f518
choiyoungjin <cyd5538@naver.com>
2022-07-03 21:20:49 +0900

    react-swiper practice

---
+++ react-swiper/src/App.js
@@ -0,0 +1,29 @@
+import { Navigation, Pagination } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import './App.css'
+const App = () => {
+  return (
+    <div className="App">
+    <Swiper
+      modules={[Navigation, Pagination]}
+      spaceBetween={50}
+      slidesPerView={3}
+      navigation
+      pagination={{ clickable: true }}
+      scrollbar={{ draggable: true }}
+    >
+      <SwiperSlide>Slide 1</SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+      <SwiperSlide>Slide 5</SwiperSlide>
+    </Swiper>
+    </div>
+  );
+};
+export default App;
