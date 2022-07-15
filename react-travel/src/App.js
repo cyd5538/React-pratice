778d47b7517f6f397cf6d7bdc496846ef8f72e84
choiyoungjin <cyd5538@naver.com>
2022-07-15 22:41:05 +0900

    react-travel

---
+++ react-travel/src/App.js
@@ -0,0 +1,23 @@
+import ScrollToTop from "./components/ScrollToTop";
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Service from "./components/Service";
+import Recommend from "./components/Recommend";
+import Testimonials from "./components/Testimonials";
+import Footer from "./components/Footer";
+
+function App() {
+  return (  
+    <div>
+      {/* <ScrollToTop /> */}
+      <Navbar />
+      <Hero />
+      {/* <Service />
+      <Recommend />
+      <Testimonials />
+      <Footer /> */}
+    </div>
+  );
+}
+
+export default App;
