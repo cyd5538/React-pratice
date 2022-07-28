2ead9900e53d2445a13f6ac723fb8375b3c6e4fa
choiyoungjin <cyd5538@naver.com>
2022-07-28 22:31:24 +0900

    submit

--- react-express-crud/client/src/App.js
+++ react-express-crud/client/src/App.js
@@ -1,19 +1,24 @@
-import {Routes,Route} from 'react-router-dom'
-import {ToastContainer} from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css';
-import About from './pages/About';
-import AddUser from './pages/AddUser';
-import Home from './pages/Home';
-import View from './pages/View';
-
+import { Routes, Route } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import Header from "./components/Header";
+import About from "./pages/About";
+import AddUser from "./pages/AddUser";
+import Home from "./pages/Home";
+import View from "./pages/View";
+import './app.css'
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/update/:id" element={<AddUser />} />
-      <Route path="/view/:id" element={<View />} />
-    </Routes>
+    <>
+      <Header />
+      <ToastContainer position="top-center" />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/add" element={<AddUser />} />
+        <Route path="/update/:id" element={<AddUser />} />
+        <Route path="/view/:id" element={<View />} />
+      </Routes>
+    </>
   );
 }
 
--- react-express-crud/client/src/pages/AddUser.js
+++ react-express-crud/client/src/pages/AddUser.js
@@ -1,9 +1,112 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { useNavigate, useLocation, useParams } from "react-router-dom";
+import axios from "axios";
+import "./AddUser.css";
+import { toast } from "react-toastify";
 
 const AddUser = () => {
-  return (
-    <div>AddUser</div>
-  )
-}
+  const [state, setState] = useState({
+    name: "",
+    email: "",
+    contact: "",
+  });
+  const { name, email, contact } = state;
 
-export default AddUser
+  const { id } = useParams();
+
+  useEffect(() => {
+    if (id) {
+      getSingleUser(id);
+    }
+  }, [id]);
+
+  const getSingleUser = async (id) => {
+    const response = await axios.get(`http:///localhost:5000/user/${id}`);
+    if (response.data === 200) {
+      setState({ ...response.data[0] });
+    }
+  };
+
+  const navigate = useNavigate();
+
+  const addUser = async (data) => {
+    const response = await axios.post("http:///localhost:5000/user", data);
+    if (response.data === 200) {
+      toast.success(response.data);
+    }
+  };
+
+  const updateUser = async (data, id) => {
+    const response = await axios.put(`http:///localhost:5000/user/${id}`, data);
+    if (response.data === 200) {
+      toast.success(response.data);
+    }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!name || !email || !contact) {
+      toast.error("넣어라좀");
+    } else {
+      if (!id) {
+        addUser(state);
+      }else{
+        updateUser(state, id)
+      }
+      navigate("/");
+    }
+  };
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setState({
+      ...state,
+      [name]: value,
+    });
+  };
+
+  return (
+    <div style={{ marginTop: "100px" }}>
+      <form
+        style={{
+          margin: "auto",
+          padding: "15px",
+          maxWidth: "400px",
+          alignContent: "center",
+        }}
+        onSubmit={handleSubmit}
+      >
+        <label htmlFor="name">Name</label>
+        <input
+          type="text"
+          id="name"
+          name="name"
+          placeholder="name"
+          onChange={handleInputChange}
+          value={name}
+        />
+        <label htmlFor="name">Email</label>
+        <input
+          type="email"
+          id="email"
+          name="email"
+          placeholder="email"
+          onChange={handleInputChange}
+          value={email}
+        />
+        <label htmlFor="name">Contact</label>
+        <input
+          type="number"
+          id="contact"
+          name="contact"
+          placeholder="contact"
+          onChange={handleInputChange}
+          value={contact}
+        />
+        <input type="submit" value={id ? "Update" : "Add"} />
+      </form>
+    </div>
+  );
+};
+
+export default AddUser;
